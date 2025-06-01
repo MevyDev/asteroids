f9ce4ed34ba71ac63a2eaa020637253fb01ea367
MevyDev <mevydev@proton.me>
2025-06-01 23:48:48 +0200

    feat: spawn and draw the player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 
@@ -12,12 +13,16 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
 
         pygame.display.flip()
         dt = game_clock.tick(FPS_CAP) / 1000
